d507c9cc9851c7b23b18a4d04350048593d633a4
unknown <974354419@qq.com>
2024-10-25 15:45:43 +0800

    finish lab4b

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            n  = n / 2;
+        } else  {
+            n = 3*n+1;
         }
+        return n;
     }
 
     public static void main(String[] args) {
@@ -24,5 +23,4 @@ public class Collatz {
         System.out.println();
     }
 }
-
 
